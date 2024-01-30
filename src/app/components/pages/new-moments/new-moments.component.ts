881035149de3abe82824a3c58b4b7d3390099c70
unknown <mauriciolobo509@gmail.com>
2024-01-30 14:00:31 -0300

    estilizando cmponent header

---
+++ src/app/components/pages/new-moments/new-moments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moments',
+  templateUrl: './new-moments.component.html',
+  styleUrls: ['./new-moments.component.css']
+})
+export class NewMomentsComponent {
+
+}
