53ed2a177c24fa077af9f5428d03c3f72bf7ee3e
unknown <mauriciolobo509@gmail.com>
2024-01-30 17:42:37 -0300

    pasta de imagens

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,45 +1,42 @@
-import { Component,Input,OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.css']
 })
-export class MomentFormComponent implements OnInit{
-  @Input() bntText!: string
-
-  momentForm!: FormGroup
-
-  constructor() {}
+export class MomentFormComponent implements OnInit {
+  @Input() bntText!: string;
+  momentForm!: FormGroup;
 
   ngOnInit(): void {
-      this.momentForm = new FormGroup({
-        id: new FormControl('', [Validators.required]),
-        title: new FormControl(''),
-        description: new FormControl('', [Validators.required]),
-        image: new FormControl(''),
-      });
+    this.momentForm = new FormGroup({
+      id: new FormControl('', [Validators.required]),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    });
   }
 
-get title(){
-  return this.momentForm.get('title')!;
-}
+  get title() {
+    return this.momentForm.get('title')!;
+  }
 
-get description(){
-  return this.momentForm.get('description')!;
-}
+  get description() {
+    return this.momentForm.get('description')!;
+  }
 
-  
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0]; // Corrigido o nome da propriedade 'files'
+    this.momentForm.patchValue({ image: file });
+  }
 
   submit() {
-if(this.momentForm.invalid){
-  return;
-}
+    if (this.momentForm.invalid) {
+      return;
+    }
 
-
-    console.log('Enviou o formulário')
+    console.log(this.momentForm.value);
   }
-
 }
