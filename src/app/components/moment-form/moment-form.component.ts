e9549d8b68a71ceabb1fdafec2b56cbdd482a4e3
unknown <mauriciolobo509@gmail.com>
2024-01-30 17:23:07 -0300

    validaçoes de form feitas

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,11 +1,45 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.css']
 })
-export class MomentFormComponent {
+export class MomentFormComponent implements OnInit{
   @Input() bntText!: string
+
+  momentForm!: FormGroup
+
+  constructor() {}
+
+  ngOnInit(): void {
+      this.momentForm = new FormGroup({
+        id: new FormControl('', [Validators.required]),
+        title: new FormControl(''),
+        description: new FormControl('', [Validators.required]),
+        image: new FormControl(''),
+      });
+  }
+
+get title(){
+  return this.momentForm.get('title')!;
+}
+
+get description(){
+  return this.momentForm.get('description')!;
+}
+
+  
+
+  submit() {
+if(this.momentForm.invalid){
+  return;
+}
+
+
+    console.log('Enviou o formulário')
+  }
 
 }
